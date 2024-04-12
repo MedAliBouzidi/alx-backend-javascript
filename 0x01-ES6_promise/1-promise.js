f6e987fcfcf2c36e41adc18d274792952379033c
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-12 19:18:07 +0200

    Fix task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  if (!success) throw new Error('The fake API is not working currently');
-  return Promise.resolve({ status: 200, body: 'success' });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
