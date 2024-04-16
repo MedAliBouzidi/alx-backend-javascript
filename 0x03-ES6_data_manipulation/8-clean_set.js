01ae321b09605932245d2cb4423aa7f1d094cc56
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-16 20:12:44 +0200

    fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,18 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (
-    !set
-    || !startString
-    || startString === undefined
-    || startString.length == 0
-  ) return '';
-
-  const newSet = new Set();
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      newSet.add(value);
-    }
-  });
-  return Array.from(newSet)
-    .map((value) => value.slice(startString.length))
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
     .join('-');
 }
