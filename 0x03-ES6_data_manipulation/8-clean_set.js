c63cdbc22018ff73020bf7ff03432a72e2e917f7
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-16 20:03:37 +0200

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (startString.length === 0 || startString === undefined) return '';
   return Array.from(set)
     .filter((item) => item.startsWith(startString))
-    .map((value) => value.substring(startString.length))
+    .map((value) => value.slice(startString.length))
     .join('-');
 }
