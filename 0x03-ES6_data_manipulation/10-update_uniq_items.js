93a54626f975955694fdcc6f96cc7fe79a045060
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-16 20:33:17 +0200

    fix task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(groceries) {
-  if (!groceries instanceof Map) throw new Error('Cannot process');
+  if (!(groceries instanceof Map)) throw new Error('Cannot process');
   for (const [key, value] of groceries.entries()) {
     if (value === 1) groceries.set(key, 100);
   }
